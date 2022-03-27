e8b0245dc19cdce529c014f120bb0636a1a95ea4
Bálint Kókai <galsaizoltan1@gmail.com>
2022-03-27 11:11:46 +0200

    Added docstrings

--- src/wraperr/wrap_error.py
+++ src/wraperr/wrap_error.py
@@ -1,3 +1,8 @@
+"""wrap_error
+=============
+"""
+
+
 import functools
 from typing import Any, Callable, Type
 
@@ -6,6 +11,24 @@ def wrap_error(
     error_handler: Callable[[BaseException], Any],
     exception_type: Type[BaseException] = Exception
 ):
+    """Parameterizes a function that can wrap another function.
+    The wrapee function might raise an exception, and when it does,
+    the wrapped function will call the specified error handler
+    if the specified exception type was raised.
+
+    :param error_handler: The specified error handler.
+    :type error_handler: Callable[[BaseException], Any]
+    :param exception_type: The specified exception type,
+                           defaults to Exception.
+    :type exception_type: Type[BaseException]
+
+    :returns: A function that takes a function as input then
+              decorates it, so that the specified error handler
+              will handle the specified exception, in case it was
+              raised during the execution of the input function.
+    :rtype: Callable[[Callable], Callable]
+    """
+
     def f(wrappee: Callable):
         @functools.wraps(wrappee)
         def g(*args, **kwargs):
